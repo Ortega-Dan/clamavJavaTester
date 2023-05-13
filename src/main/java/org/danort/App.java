44223cf5b40da3e976b954b2514bf38c99500fc1
Dan Ortega <ortega.dan2010@gmail.com>
2023-05-12 23:27:36 -0500

    some attempted work with notifications

--- src/main/java/org/danort/App.java
+++ src/main/java/org/danort/App.java
@@ -4,14 +4,19 @@ package org.danort;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+
 import com.google.cloud.NoCredentials;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Notification;
+import com.google.cloud.storage.NotificationInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.NotificationInfo.EventType;
+import com.google.cloud.storage.NotificationInfo.PayloadFormat;
 
 // import xyz.capybara.clamav.ClamavClient;
 // import xyz.capybara.clamav.commands.scan.result.ScanResult;
@@ -31,32 +36,53 @@ public class App {
 
             // ScanResult result = client.scan(fis);
 
-            // System.out.println(" ****** " + result.getClass().getSimpleName() + " ****** ");
+            // System.out.println(" ****** " + result.getClass().getSimpleName() + " ******
+            // ");
         }
 
         // CLOUDSTORAGE
         {
             String fakeGcsExternalUrl = "http://localhost:4443";
 
-            Storage storageClient = StorageOptions.newBuilder()
+            String projectId = "dsgov-dev";
+            String bucketName = "bucketName";
+            String topicName = "projects/dsgov-dev/topics/NEW_FILE_UPLOAD";
+
+            Storage storage = StorageOptions.newBuilder()
                     .setHost(fakeGcsExternalUrl)
-                    // .setProjectId("test-project")
+                    .setProjectId(projectId)
                     .setCredentials(NoCredentials.getInstance())
                     .build()
                     .getService();
 
-            Bucket bucket = storageClient.get("sample-bucket2");
+            Bucket bucket = storage.get(bucketName);
             if (bucket == null) {
                 // Create a new bucket
-                storageClient.create(BucketInfo.newBuilder("sample-bucket2").build());
+                storage.create(BucketInfo.newBuilder(bucketName).build());
             }
-            WriteChannel channel = storageClient
-                    .writer(BlobInfo.newBuilder("sample-bucket2", "some_file2.txt").build());
+
+            // configuring the bucket to send notifications to a topic
+            {
+                // Create the notification
+                // NotificationInfo notificationInfo = NotificationInfo.newBuilder(topicName)
+                //         .setPayloadFormat(PayloadFormat.JSON_API_V1).setEventTypes(EventType.OBJECT_FINALIZE)
+                //         .build();
+
+                // Notification notification = storage.createNotification(bucketName, notificationInfo);
+
+                // // just showing data
+                // String topic = notification.getTopic();
+                // System.out.println("Successfully created notification for topic " + topic);
+
+            }
+
+            WriteChannel channel = storage
+                    .writer(BlobInfo.newBuilder(bucketName, "some_file2.txt").build());
             channel.write(ByteBuffer.wrap("line3\n".getBytes()));
             channel.write(ByteBuffer.wrap("line4\n".getBytes()));
             channel.close();
 
-            Blob someFile2 = storageClient.get("sample-bucket2", "some_file2.txt");
+            Blob someFile2 = storage.get(bucketName, "some_file2.txt");
             String fileContent = new String(someFile2.getContent());
 
             System.out.println(fileContent);
